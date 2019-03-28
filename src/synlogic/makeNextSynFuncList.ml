143eeca92711e6773473742682c18643591be8be
jisung hong <rhfktj@gmail.com>
2019-03-28 23:06:18 +0900

    Add make next fun body from term module

---
+++ src/synlogic/makeNextSynFuncList.ml
@@ -0,0 +1,51 @@
+open Ast
+
+let rec combinationTwoList li lili =
+  match li with
+  | [] -> []
+  | h::t ->
+    let rec combinationList lili e =
+      match lili with
+      | [] -> []
+      | h::t ->
+        (e::h)::combinationList t e
+    in
+    (combinationList lili h) @ (combinationTwoList t lili)
+
+let rec combIdenTermList idenlist termlist =
+  match idenlist with
+  | [] -> []
+  | h::t ->
+    let rec combtermlist termlist iden = 
+      match termlist with
+      | [] -> []
+      | h::t ->
+        (IdentifierTerms(iden, h))::(combtermlist t iden)
+    in
+      (combtermlist termlist h) @ (combIdenTermList t termlist)
+
+(* just one function *)
+let rec makeNextSynFuncList term hash = 
+  match term with
+  | Identifier identifier -> 
+    (try
+      Hashtbl.find hash (Identifier(identifier))
+    with
+      _ -> [Identifier(identifier)]
+    )
+  | Literal literal -> [Literal(literal)]
+  | IdentifierTerms (identifier, termlist) ->
+    let rec makeTermlist termlist =
+      match termlist with
+      | [] -> [[]]
+      | h::t ->
+        let caselistiden = makeNextSynFuncList h hash in
+        let caselistterm = makeTermlist t in
+        combinationTwoList caselistiden caselistterm
+    in 
+      let termlistcase = makeTermlist termlist in
+      combIdenTermList [identifier] termlistcase
+     
+  | Exists (sortedvarlist, term) -> [Exists(sortedvarlist, term)]
+  | Forall (sortedvarlist, term) -> [Forall(sortedvarlist, term)]
+  | Let (varbindinglist, term) -> [Let(varbindinglist, term)]
