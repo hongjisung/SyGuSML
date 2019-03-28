ee7557b04abae87c732f5ec05360685d1c706117
jisung hong <rhfktj@gmail.com>
2019-03-28 23:07:06 +0900

    Add change synth-fun to define-fun module

---
+++ src/synlogic/changeSynfunToDefFun.ml
@@ -0,0 +1,31 @@
+open Ast
+(* now, only one function *)
+let changeSynfunToDefFun parsetree deffun=
+  let rec analysisCmd parsetree  = 
+    match parsetree with
+    | [] -> []
+    | h::t ->
+      match h with
+      | CheckSynth -> h::(analysisCmd t)
+      | Constraint _-> h::(analysisCmd t)
+      | DeclareVar (symbol, sort) -> h::(analysisCmd t)
+      | InvConstraint _ -> h::(analysisCmd t)
+      | SetFeature (f, b) -> h::(analysisCmd t)
+      | SynthFun (symbol, sortedvarlist, sort, grammardefopt) -> deffun::(analysisCmd t)
+      | SynthInv (symbol, sortedvarlist, grammardefopt) ->         
+        analysisCmd (SynthFun(symbol, sortedvarlist, Sort(SymbolIdentifier(Symbol("Bool"))), grammardefopt)::t)
+      | SmtCmd smt_cmd -> (
+        match smt_cmd with
+        | DeclareDatatype (symbol, dtdec) -> 
+          analysisCmd (SmtCmd(DeclareDatatypes([(SortDeclaration(symbol, "0"), dtdec)]))::t)
+        | DeclareDatatypes dtlist -> h::(analysisCmd t)
+        | DeclareSort (symbol, numeral)  -> h::(analysisCmd t)
+        | DefineFun (symbol, sortedvarlist, sort, term) -> h::(analysisCmd t)
+        | DefineSort (symbol, sort) -> h::(analysisCmd t)
+        | SetLogic symbol -> h::(analysisCmd t)
+        | SetOption (symbol, literal) -> h::(analysisCmd t)
+      ) 
+  in 
+  let newpst = analysisCmd parsetree in 
+    (* Printf.printf "synToDef: %b\n\n" (newpst = HandmadeOutput.changesynfuntest); *)
+    newpst
