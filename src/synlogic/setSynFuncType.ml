a0d0257d051d0bed4e778b45afb3cc2fcf47ad18
jisung hong <rhfktj@gmail.com>
2019-03-28 23:03:20 +0900

    Add synfun type file

---
+++ src/synlogic/setSynFuncType.ml
@@ -0,0 +1,7 @@
+open Ast
+type signature = 
+  | SortSignature of sort
+  | SymbolSignature of symbol
+
+type funcIngredient =
+  | FuncIngredient of (symbol * sorted_var list * sort * term * (term, term list) Hashtbl.t)
