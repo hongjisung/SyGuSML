fffa27f8430db523d5eaacf54957c1ca9b3cd153
chacham <ccm0702@naver.com>
2019-04-28 17:12:04 +0900

    Refactor astToZ3String

--- src/synlogic/astToZ3string.ml
+++ src/synlogic/astToZ3string.ml
@@ -19,55 +19,55 @@ open AstToSygusString
       *SETLOGIC -> 
       *SETOPTION -> 
 *)
-let rec analysisCmd parsetree vars =
+let rec astToZ3StringList ast vars =
   (* Printf.printf "change to z3 string: %d\n" (List.length parsetree); *)
-  match parsetree with
+  match ast with
   | [] -> []
   | h::t ->
     match h with
     | CheckSynth -> 
-      "(check-sat)"::(analysisCmd t vars)
+      "(check-sat)"::(astToZ3StringList t vars)
     | Constraint term ->
       (* let str = "" in *)
       (* let str = String.concat " " ("(assert"::(termToString term)::[")"]) in *)
       if List.length vars > 0 then
         let varsStr = String.concat " " (["("] @ vars @ [")"]) in
         let str = String.concat " " ["(assert"; "(forall"; varsStr; (termToString term); "))"] in
-        str::(analysisCmd t vars)
+        str::(astToZ3StringList t vars)
       else
         let str = String.concat " " ("(assert"::(termToString term)::[")"]) in
-        str::(analysisCmd t vars)
+        str::(astToZ3StringList t vars)
     | DeclareVar (symbol, sort) -> 
       let symbolStr = symbolToString symbol in
       let sortStr = sortToString sort in
       let str = String.concat " " ("(declare-const"::symbolStr::sortStr::[")"]) in
       let sortVar = String.concat " " ["("; symbolStr; sortStr; ")"] in
-      str::(analysisCmd t (vars @ [sortVar]))
-    | InvConstraint _ -> (analysisCmd t vars)
-    | SetFeature (f, b) -> (analysisCmd t vars)
-    | SynthFun (symbol, sortedvarlist, sort, grammardefopt) -> (analysisCmd t vars)
+      str::(astToZ3StringList t (vars @ [sortVar]))
+    | InvConstraint _ -> (astToZ3StringList t vars)
+    | SetFeature (f, b) -> (astToZ3StringList t vars)
+    | SynthFun (symbol, sortedvarlist, sort, grammardefopt) -> (astToZ3StringList t vars)
     | SynthInv (symbol, sortedvarlist, grammardefopt) ->         
-      analysisCmd (SynthFun(symbol, sortedvarlist, Sort(SymbolIdentifier(Symbol("Bool"))), grammardefopt)::t) vars
+      astToZ3StringList (SynthFun(symbol, sortedvarlist, Sort(SymbolIdentifier(Symbol("Bool"))), grammardefopt)::t) vars
     | SmtCmd smt_cmd -> (
         match smt_cmd with
         | DeclareDatatype (symbol, dtdec) -> 
-          analysisCmd (SmtCmd(DeclareDatatypes([(SortDeclaration(symbol, "0"), dtdec)]))::t) vars
+          astToZ3StringList (SmtCmd(DeclareDatatypes([(SortDeclaration(symbol, "0"), dtdec)]))::t) vars
         | DeclareDatatypes dtlist ->
-          (smtcmdToString smt_cmd)::(analysisCmd t vars)
+          (smtcmdToString smt_cmd)::(astToZ3StringList t vars)
         | DeclareSort (symbol, numeral)  -> 
-          (smtcmdToString smt_cmd)::(analysisCmd t vars)
+          (smtcmdToString smt_cmd)::(astToZ3StringList t vars)
         | DefineFun (symbol, sortedvarlist, sort, term) -> 
-          (smtcmdToString smt_cmd)::(analysisCmd t vars)
+          (smtcmdToString smt_cmd)::(astToZ3StringList t vars)
         | DefineSort (symbol, sort) ->  
-          (smtcmdToString smt_cmd)::(analysisCmd t vars)
+          (smtcmdToString smt_cmd)::(astToZ3StringList t vars)
         | SetLogic symbol ->  
-          (smtcmdToString smt_cmd)::(analysisCmd t vars)
+          (smtcmdToString smt_cmd)::(astToZ3StringList t vars)
         | SetOption (symbol, literal) ->  
-          (smtcmdToString smt_cmd)::(analysisCmd t vars)
+          (smtcmdToString smt_cmd)::(astToZ3StringList t vars)
       )
 (* now, only one function *)
-let astToZ3string parsetree =
-  let newstringlist = analysisCmd parsetree [] in
+let astToZ3string ast =
+  let newstringlist = astToZ3StringList ast [] in
   let newstring = (String.concat "\n" newstringlist) in
   (* print_endline "change to z3 string: ";
      print_endline (newstring);
