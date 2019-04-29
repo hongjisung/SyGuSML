3c3aae638a6009cfc5894037cc482391635fbb63
jisung hong <rhfktj@gmail.com>
2019-04-29 15:18:19 +0900

    Correct doc

--- src/solver.ml
+++ src/solver.ml
@@ -1,18 +1,15 @@
 (**
-   Find the correct function satisfy sygus language problem 
+   Find the correct function satisfy sygus language problem
+
    1. read example string from file
+
    2. change it to parse tree
+
    3. get syn-fun grammar from the tree
+
    4. get syn-fun ingredient hash from syn-fun grammar
+
    5. make fun with grammar by search algorithm
-
-   $ eval $(opam env)
-   $ export LD_LIBRARY_PATH=/newdisk/synKU/_opam/lib/z3
-
-   how to set cost
-
-   Instead of check sat, check unsat for not expr.
-   Because solver find result for special case of variable.
 *)
 
 open Ast
@@ -30,7 +27,7 @@ let examples = ["./benchmarks/example1"]
 
 (** Find the correct function in examples.
     @param examples example file list 
-    @return the result sygus string
+    @return the result sygus string list
 *)
 let rec solveExamples examples =
   match examples with
