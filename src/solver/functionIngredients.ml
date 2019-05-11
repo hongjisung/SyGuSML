01aee73a65035a619f42198e877de57361dfc397
chacham <ccm0702@naver.com>
2019-05-11 18:16:00 +0900

    Works with Map

--- src/solver/functionIngredients.ml
+++ src/solver/functionIngredients.ml
@@ -1,42 +1,9 @@
-(*
-1. get function name : symbol
-2. get parameter :sortedvar list
-3. get output sort : sort
-4. get Start Placeholder : term(sort)
-5. get hashmap : term(sortname, varname) * term list
-*)
 open Ast
 open IntermediateTypes
 exception GTVariableError
 exception SynthFunInputError
 
-(* let rec addSortedvarlistToHash sortedvarlist hash = 
-   match sortedvarlist with
-   | [] -> hash
-   | h::t ->
-    match h with
-    | SortedVar(sym, sort) ->
-      (
-        try
-          let li = Hashtbl.find hash (Terms.sortToTerm sort) in 
-          Hashtbl.add hash (Terms.sortToTerm sort) (Identifier(SymbolIdentifier(sym))::li) 
-        with _ -> Hashtbl.add hash (Terms.sortToTerm sort) [Identifier(SymbolIdentifier(sym))]
-      );
-      addSortedvarlistToHash t hash *)
-
-let addSortedVar sortedvar transitionMap =
-  let SortedVar(sym, sort) = sortedvar in
-  let key = (Terms.sortToTerm sort) in
-  let oldTransition = TransitionMap.find_default [] key transitionMap in
-  let newTransition = (Identifier(SymbolIdentifier(sym)))::oldTransition in
-  TransitionMap.add key newTransition transitionMap
-
-let rec addSortedVars sortedvars transitionMap =
-  match sortedvars with
-  | [] -> transitionMap
-  | sotredvar::t -> addSortedVars t (addSortedVar sotredvar transitionMap)
-
-let rec changeBftermToTerm bfterm =
+let rec bftermToTerm bfterm =
   match bfterm with
   | BfIdentifier identifier -> Identifier(identifier)
   | BfLiteral literal -> Literal(literal)
@@ -45,36 +12,28 @@ let rec changeBftermToTerm bfterm =
       match bftermlist with
       | [] -> []
       | h::t ->
-        (changeBftermToTerm h) :: (changeBftermlistToTerm t)
+        (bftermToTerm h) :: (changeBftermlistToTerm t)
     in 
     IdentifierTerms(identifier, changeBftermlistToTerm bftermlist)
 
-(* let rec addGtermlistToHash gtermlist key hash =
-   match gtermlist with
-   | [] -> hash
-   | h::t ->
-    match h with 
-    (* don't treat now *)
-    | GTConstant sort -> addGtermlistToHash t key hash
-    (* this should never appear *)
-    | GTVariable sort -> raise GTVariableError
-    | GTBfTerm bf_term ->(
-        let newterm = changeBftermToTerm bf_term in
-        (
-          try
-            let li = Hashtbl.find hash key in 
-            Hashtbl.add hash key (newterm::li)
-          with _ -> Hashtbl.add hash key [newterm]
-        );
-        addGtermlistToHash t key hash
-      ) *)
+let addSortedVar sortedvar transitionMap =
+  let SortedVar(symbol, sort) = sortedvar in
+  let key = (Terms.sortToTerm sort) in
+  let oldTransition = TransitionMap.find_default [] key transitionMap in
+  let newTransition = (Identifier(SymbolIdentifier(symbol)))::oldTransition in
+  TransitionMap.add key newTransition transitionMap
+
+let rec addSortedVars sortedvars transitionMap =
+  match sortedvars with
+  | [] -> transitionMap
+  | sotredvar::t -> addSortedVars t (addSortedVar sotredvar transitionMap)
 
 let addGterm key gterm transitionMap =
   match gterm with
   | GTConstant sort -> transitionMap
   | GTVariable sort -> raise GTVariableError
   | GTBfTerm bfTerm ->
-    let term = changeBftermToTerm bfTerm in
+    let term = bftermToTerm bfTerm in
     let oldTransition = TransitionMap.find_default [] key transitionMap in
     let newTransition = term::oldTransition in
     TransitionMap.add key newTransition transitionMap
@@ -85,17 +44,6 @@ let rec addGterms key gterms transitionMap =
   | gterm::t ->
     addGterms key t (addGterm key gterm transitionMap)
 
-(* let rec addGrammarlistToHash grammarlist hash =
-   let hashref = ref (hash) in
-   match grammarlist with
-   | [] -> !hashref
-   | h::t ->
-    match h with
-    | (sortedvar, GroupedRuleList(symbol, sort, gtermlist)) ->
-      hashref := addSortedvarlistToHash [sortedvar] !hashref;
-      hashref := addGtermlistToHash gtermlist (Identifier(SymbolIdentifier(symbol))) !hashref;
-      addGrammarlistToHash t !hashref *)
-
 let rec addGrammars grammars transitionMap =
   match grammars with
   | [] -> transitionMap
@@ -104,31 +52,17 @@ let rec addGrammars grammars transitionMap =
     let transitionMapWithGTerms = addGterms (Identifier(SymbolIdentifier(symbol))) gtermlist transitionMapWithSortedVar in
     addGrammars t transitionMapWithGTerms
 
-(* let getFunctionIngredient synfun =
-   let hashref = ref (Hashtbl.create 31) in
-   match synfun with
-   | SynthFun (funcname, sortedvarlist, sort, grammardefopt) ->
-    hashref := addSortedvarlistToHash sortedvarlist !hashref;
-    let output = Terms.sortToTerm sort in (
-      match grammardefopt with 
-      | None -> FuncIngredient(funcname, sortedvarlist, sort, output, !hashref)
-      | Some GrammarDef(grammarlist) ->
-        hashref := addGrammarlistToHash grammarlist !hashref;
-        FuncIngredient(funcname, sortedvarlist, sort, output, !hashref)
-    )
-   | _ -> raise SynthFunInputError *)
-
 let getFunctionIngredient synfun =
   match synfun with
-  | SynthFun (fname, sortedvars, sort, grammardefOption) ->
+  | SynthFun (fname, parameters, outputSort, grammardefOption) ->
     (
-      let output = Terms.sortToTerm sort in
-      let transitionMapWithSortedVars = addSortedVars sortedvars TransitionMap.empty in
+      let start = Terms.sortToTerm outputSort in
+      let transitionMapWithSortedVars = addSortedVars parameters TransitionMap.empty in
       match grammardefOption with
       | Some GrammarDef(grammars) ->
         let transitionMapWithGrammars = addGrammars grammars transitionMapWithSortedVars in
-        FuncIngredient(fname, sortedvars, sort, output, transitionMapWithGrammars)
-      | None -> FuncIngredient(fname, sortedvars, sort, output, transitionMapWithSortedVars)
+        FuncIngredient(fname, parameters, outputSort, start, transitionMapWithGrammars)
+      | None -> FuncIngredient(fname, parameters, outputSort, start, transitionMapWithSortedVars)
     )
   | _ -> raise SynthFunInputError
 
--- src/types/intermediateTypes.ml
+++ src/types/intermediateTypes.ml
@@ -1,21 +1,11 @@
 open Ast
 
-(* module WorklistBFS = struct
-   type t = Ast.term BatDeque.t
-   let empty = BatDeque.empty
-   let is_empty = BatDeque.is_empty
-   let add a l = BatDeque.snoc l a
-   let add_list al l = List.fold_right add al l
-   let choose l = BatDeque.front l
-   let size l = BatDeque.size l
-   end *)
-
-module OrderedType = struct
+module NonTerminal = struct
   type t = term
   let compare t1 t2 = String.compare (Stringfier.termToSygusString t1) (Stringfier.termToSygusString t2)
 end
 
-module TransitionMap = BatMap.Make(OrderedType)
+module TransitionMap = BatMap.Make(NonTerminal)
 
 type signature = 
   | SortSignature of sort
