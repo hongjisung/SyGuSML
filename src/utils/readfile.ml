eae9cd51ed9e9fd176234b92f387ab3453bee82b
jisung hong <rhfktj@gmail.com>
2019-05-18 16:46:04 +0900

    Add close_in of file

--- src/utils/readfile.ml
+++ src/utils/readfile.ml
@@ -9,5 +9,7 @@ let readfile name =
       let line = input_line channel in
       line::(readtext channel)
     with
-      End_of_file -> []
+      End_of_file ->
+      close_in channel;
+      []
   in String.concat "\n" (readtext channel) 
