b8cd7143bfd239a1fefc713e1b19439b4b7b88d5
cushionbadak <jsbzwork@gmail.com>
2019-08-12 08:21:40 +0000

    proceed making Z3solver : deal with synthed function

--- src/tools/inner/z3solverInn.ml
+++ src/tools/inner/z3solverInn.ml
@@ -1,6 +1,7 @@
 open Ast
 
 exception UnexpectedCmdException
+exception UnexpectedSmtCmdException
 
 (****************************)
 (* collect sorted_var list. *)
@@ -120,3 +121,78 @@ let cmd_to_Z3String : cmd -> string =
 let convert_Z3LikelyAst_to_Z3String : cmd list -> string =
   fun clist ->
   List.fold_left (fun acc c -> acc ^ (cmd_to_Z3String c)) "" clist
+
+
+(*****************************************************)
+(* Make Z3 compatible string from 'synthed-function' *)
+(*****************************************************)
+(*
+  Synthesized functions, which is the most important part of 'SyGuS' problem,
+  is to be inserted at the end of the Z3 solver as 'assert' expression.
+  'define-fun' is the syntax sugar of ('declare-fun', 'assert') pair.
+  (reference: https://stackoverflow.com/a/7745041/10353572)
+  So the function 'cmd_to_Z3String' constructs 'declare-fun'
+  and this functions constructs 'assert'.
+*)
+(*
+  Example:
+
+  Target 'synth-fun' in SyGuS:
+    (synth-fun f ((x Int) (y Int)) Int (<<GRAMMAR-DEF>>))
+  Predeclared 'synth-fun' in Z3 (using 'cmd_to_Z3String'):
+    (declare-fun f ((x Int) (y Int)) Int)
+
+  'synthed_fun_to_Z3String' Example input:
+    (define-fun f ((x Int) (y Int)) Int
+      (+ ( * 2 x) ( * 2 y))
+    )
+  'synthed_fun_to_Z3String' Example output:
+    (assert 
+      (forall ((x Int) (y Int))
+        ( = (f x y) (+ ( * 2 x) ( * 2 y)) )
+      )
+    )
+*)
+
+(* helper functions *)
+let gen_id : string -> Ast.identifier = 
+  fun s -> SymbolIdentifier (Symbol s)
+let collect_args : Ast.sorted_var list -> Ast.term list =
+  fun svl ->
+  List.map (fun (SortedVar (Symbol s, srt)) -> Identifier (gen_id s)) svl
+let make_applyTerm : Ast.symbol -> Ast.sorted_var list -> Ast.term = 
+  fun (Symbol s) svl ->
+  IdentifierTerms ((gen_id s), (collect_args svl))
+
+let define_fun_to_constraint_inner : (Ast.symbol * Ast.sorted_var list * Ast.sort * Ast.term) -> Ast.cmd =
+  fun (sy, svl, s, t) ->
+  let newterm : Ast.term =
+    Forall (svl, IdentifierTerms ((gen_id "="), [(make_applyTerm sy svl); t]))
+  in
+  Constraint newterm
+
+let define_fun_to_constraint : Ast.smt_cmd -> Ast.cmd =
+  fun sc ->
+  match sc with
+  | DefineFun (sy, svl, s, t) -> define_fun_to_constraint_inner (sy, svl, s, t)
+  | _ -> raise UnexpectedSmtCmdException
+
+let synthed_fun_to_Z3String : Ast.smt_cmd -> string =
+  fun sc ->
+  let converted_constraint : Ast.cmd = define_fun_to_constraint sc in
+  cmd_to_Z3String converted_constraint
+
+(* 'synthed_fun_to_Z3String' calls 'List.map' in 'collect_args' for the same arguments. 
+    If you want to evaluate the duplicated parts only once, use following fuctions.
+*)
+let synthed_fun_constraint_bake_head : Ast.smt_cmd -> Ast.cmd = function
+  | DefineFun (sy, svl, s, t) ->
+    Constraint (Forall (svl, IdentifierTerms ((gen_id "="), [(make_applyTerm sy svl)])))
+  | _ -> raise UnexpectedSmtCmdException
+
+let append_synthed_term_to_baked_head : Ast.cmd -> Ast.term -> Ast.cmd = 
+  fun c t ->
+  match c with
+  | Constraint (Forall (svl, IdentifierTerms ((id_head), [id_tail_1]))) ->
+    Constraint (Forall (svl, IdentifierTerms ((id_head, [id_tail_1; t]))))
+  | _ -> raise UnexpectedCmdException
